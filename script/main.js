75ac3f8bfd49a4c225c25a50727f548f27acb647
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-10-14 15:36:58 -0300

    :recycle: Refatorado o resultado em texto do imc

--- script/main.js
+++ script/main.js
@@ -26,17 +26,39 @@ buttonCalculate.onclick = () => {
     const result = imc.calculateResult().toFixed(1);
     let typeResult = "";
 
-    if(result < 18.5) {
-        typeResult = "Magreza";
-    } else if(result >= 18.5 && result <= 24.9) {
-        typeResult = "Normal";
-    } else if(result >= 25.0 && result <= 29.9) {
-        typeResult = "Sobrepeso";
-    } else if(result >= 30.0 && result <= 39.9) {
-        typeResult = "Obesidade;"
-    } else {
-        typeResult = "Obesidade Grave"
+    const conditionalsIMC = {
+        thinness: () => {
+            if(result < 18.5) {
+                typeResult = "Magreza";
+            }
+        },
+        normal: () => {
+            if(result >= 18.5 && result <= 24.9) {
+                typeResult = "Normal";
+            }
+        },
+        overWeight: () => {
+            if(result >= 25.0 && result <= 29.9) {
+                typeResult = "Sobrepeso";
+            }
+        },
+        obesity: () => {
+            if(result >= 30.0 && result <= 39.9) {
+                typeResult = "Obesidade;"
+            }
+        },
+        severeObesity: () => {
+            if (result >= 40.0){
+                typeResult = "Obesidade Grave"
+            }
+        }
     }
+
+    conditionalsIMC.thinness();
+    conditionalsIMC.normal();
+    conditionalsIMC.overWeight();
+    conditionalsIMC.obesity();
+    conditionalsIMC.severeObesity();
     
     textResult.innerText = `${result} ${typeResult}`;
 }
